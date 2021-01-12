3f252041b154b5132ce7e042230bfd9a8a9b66ea
srirag-vuppala <vuppala@calpoly.edu>
2021-01-11 16:13:49 -0800

    Finished!

--- sample_backend.py
+++ sample_backend.py
@@ -1,5 +1,6 @@
-from flask import request
 from flask import Flask
+from flask import request
+from flask import jsonify 
 app = Flask(__name__)
 
 users = { 
@@ -33,15 +34,34 @@ users = {
    ]
 }
 
-@app.route('/users/<id>')
-def get_users(id):
-    if id:
-        for user in users['users_list']:
-            if user['id'] == id:
-                return user
-        #if wrong user id is given to us return null {}
-        return ({}) 
-    return users
+
+@app.route('/users', methods=['GET', 'POST', 'DELETE'])
+def get_users():
+    if request.method == 'GET':
+        search_username = request.args.get('name')
+        search_job= request.args.get('job')
+        if search_username and search_job:
+            subdict = {'users_list' : []}
+            for user in users['users_list']:
+                if user['name'] == search_username and user['job'] == search_job:
+                    subdict['users_list'].append(user)
+            return subdict
+        return users
+    elif request.method == 'POST':
+        userToAdd = request.get_json()
+        users['users_list'].append(userToAdd)
+        resp = jsonify(success=True)
+        #resp.status_code = 200 #optionally, you can always set a response code. 
+        # 200 is the default code for a normal response
+        return resp
+    elif request.method == 'DELETE':
+        username_delete = request.args.get('name')
+        if username_delete:
+            for user in users['users_list']:
+                if user['name'] == username_delete:
+                    users.pop(user)
+        resp = jsonify(success=True)
+        return resp
 
 @app.route('/')
 def hello_world():
